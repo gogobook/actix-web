a4ab38b2cb58d452a6dd41be6483fc5a9b32bf0a
Travis CI User <travis@example.org>
2018-01-08 03:56:03 +0000

    Documentation upload

---
+++ implementors/actix/actor/trait.Actor.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = ["impl&lt;T, A, H, U, V&gt; Actor for <a class=\"struct\" href=\"actix_web/struct.HttpServer.html\" title=\"struct actix_web::HttpServer\">HttpServer</a>&lt;T, A, H, U&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;A: 'static,<br>&nbsp;&nbsp;&nbsp;&nbsp;T: IoStream,<br>&nbsp;&nbsp;&nbsp;&nbsp;H: <a class=\"trait\" href=\"actix_web/dev/trait.HttpHandler.html\" title=\"trait actix_web::dev::HttpHandler\">HttpHandler</a>,<br>&nbsp;&nbsp;&nbsp;&nbsp;U: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a>&lt;Item = V&gt; + 'static,<br>&nbsp;&nbsp;&nbsp;&nbsp;V: <a class=\"trait\" href=\"actix_web/dev/trait.IntoHttpHandler.html\" title=\"trait actix_web::dev::IntoHttpHandler\">IntoHttpHandler</a>&lt;Handler = H&gt;,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
